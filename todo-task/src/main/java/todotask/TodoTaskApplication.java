7bdaec08d5110ef98be100d0ad537d746617b544
artursbm <artursbmello@gmail.com>
2017-10-25 00:53:36 -0200

    tentando upar fotos pro server

--- todo-task/src/main/java/todotask/TodoTaskApplication.java
+++ todo-task/src/main/java/todotask/TodoTaskApplication.java
@@ -2,13 +2,15 @@
 
 package todotask;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-// import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
 @ComponentScan
